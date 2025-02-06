e5f0370a096f751696b14c5490b5cb7838afdf82
Geoff-Robin <jeffrobin132004@gmail.com>
2025-02-06 20:37:29 +0530

    Are commit messages really necessary?

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,32 +1,7 @@
-import path from 'path'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-        // Add this section to handle cookies
-        onOpen: (proxy, req, socket) => {
-          proxy.events.on('response', (proxy, req, res) => {
-            if (res.headers['set-cookie']) {
-              res.headers['set-cookie'] = res.headers['set-cookie'].map(cookie => {
-                // Add the partitioned attribute to cookies
-                return cookie + '; Partitioned'
-              })
-            }
-          })
-        }
-      }
-    }
-  }
 })
